99a2298bcdb69eb9c99eee6186baa442ed747822
Ccccraz <hy.ccccr@gmail.com>
2023-06-13 16:01:38 +0800

    切换python lsp 到 pylsp

--- shell/PowerShell/Microsoft.PowerShell_profile.ps1
+++ shell/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,6 @@ $env:HTTP_PROXY = "http://127.0.0.1:20172"
 $env:HTTPS_PROXY = "http://127.0.0.1:20172"
 $env:ALL_PROXY = "http://127.0.0.1:20172"
 
-# Env
-micromamba activate dev
-
 # Alias
 Set-Alias mamba micromamba
 
