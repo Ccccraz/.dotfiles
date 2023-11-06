8f29661a683e246170820fc1b138376404d1161d
Hu Yang <hy.ccccr@gmail.com>
2023-11-06 18:41:46 +0800

    wezterm

--- terminal/wezterm/wezterm.lua
+++ terminal/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
@@ -6,12 +6,12 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Tokyo Night (Gogh)'
-config.default_prog = { "C:/Program Files/nu/bin/nu.exe", "-l" }
+config.color_scheme = "Tokyo Night (Gogh)"
+config.default_prog = { "pwsh.exe", "-l" }
 
-config.font = wezterm.font_with_fallback {
-	"Cascadia Code",
-	"CaskaydiaCove Nerd Font Mono",
-}
+config.font = wezterm.font_with_fallback({
+	"Cascadia Code PL",
+	"等距更纱黑体 SC"
+})
 
 return config
