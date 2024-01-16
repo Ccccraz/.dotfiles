0c4c4f10a432c1443d3e50838310976e1fcd8360
Ccccraz <hy.ccccr@gmai.com>
2024-01-16 20:39:58 +0800

    init micromamba for powershell

--- shell/PowerShell/profile.ps1
+++ shell/PowerShell/profile.ps1
@@ -1,1 +1,8 @@
-(& $env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -p $env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
+
+
+#region mamba initialize
+# !! Contents within this block are managed by 'mamba shell init' !!
+$Env:MAMBA_ROOT_PREFIX = "C:\Users\ccccr\micromamba"
+$Env:MAMBA_EXE = "C:\Users\ccccr\AppData\Local\Microsoft\WinGet\Links\micromamba.exe"
+(& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -p $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
+#endregion
