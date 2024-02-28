c9536e93bd28568da9732618bec4bc6c395cc0bd
Ccccraz <hy.ccccr@gmail.com>
2024-02-29 00:24:30 +0800

    chore: Activate base on pwsh start

--- shell/PowerShell/Microsoft.PowerShell_profile.ps1
+++ shell/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,12 @@
 # Proxy
 $env:HTTP_PROXY = "http://127.0.0.1:20172"
 $env:HTTPS_PROXY = "http://127.0.0.1:20172"
-$env:ALL_PROXY = "http://127.0.0.1:20172"
 
 # Promot
 Invoke-Expression (&starship init powershell)
 
 Set-Alias -Name get -Value aria2c
 Set-Alias -Name mamba -Value micromamba
+
+mamba activate base
 
