d9af6f10982ef81981c407e24192fc3a489f4492
ccccr <hy.ccccr@gmail.com>
2024-06-19 20:02:37 +0800

    chore: :bug: back to stable vscode

--- shell/PowerShell/Microsoft.PowerShell_profile.ps1
+++ shell/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -7,7 +7,6 @@ Invoke-Expression (&starship init powershell)
 
 Set-Alias -Name get -Value aria2c
 Set-Alias -Name mamba -Value micromamba
-Set-Alias -Name code -Value code-insiders
 
 # Activate mamba base environment
 mamba activate base
