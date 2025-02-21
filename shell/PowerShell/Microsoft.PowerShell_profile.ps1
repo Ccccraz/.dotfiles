696c6d19b8b53e4c657f0ce4fdcbd512614e56e8
HuYang <hy.ccccr@gmail.com>
2025-02-21 23:40:51 +0800

    feat: :sparkles: more intelligence suggestion control

--- shell/PowerShell/Microsoft.PowerShell_profile.ps1
+++ shell/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,11 @@
 $env:HTTP_PROXY = "http://127.0.0.1:20172"
 $env:HTTPS_PROXY = "http://127.0.0.1:20172"
 
+# keymap
+Set-PSReadLineKeyHandler -Chord Ctrl+UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord Ctrl+DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord Ctrl+RightArrow -Function AcceptNextSuggestionWord
+
 # Promot
 Invoke-Expression (&starship init powershell)
 
